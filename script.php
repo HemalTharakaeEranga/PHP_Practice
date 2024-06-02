84538e2b0d55ba5fd382f43226a28417398bb0fc
HemalTharakaEranga <hemalgithub@gmail.com>
2024-06-02 13:45:16 +0530

    Varibles to PHP

---
+++ script.php
@@ -0,0 +1,10 @@
+<?php
+//variable check of output
+$str = "Hello Variables";
+$num = 28;
+$float = 60.9;
+
+echo "<h1>" .$str . "<h1>";
+echo "<h2>" .$num . "<h2>";
+echo "<h3>" .$float . "<h3>";
+?>
