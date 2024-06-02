7556936e6387992f916dc085901c0fb2b83b138d
HemalTharakaEranga <hemalgithub@gmail.com>
2024-06-02 14:10:01 +0530

    modified this use array

--- script.php
+++ script.php
@@ -1,10 +1,13 @@
 <?php
 //variable check of output
-$str = "Hello Variables";
+/*$str = "Hello Variables";
 $num = 28;
 $float = 60.9;
 
 echo "<h1>" .$str . "<h1>";
 echo "<h2>" .$num . "<h2>";
-echo "<h3>" .$float . "<h3>";
+echo "<h3>" .$float . "<h3>"; */ // multiple comments
+
+$cricket_teams = array("Sri Lanka","India","Australia","New Zeland");
+echo var_dump($cricket_teams); // this is the array use in PHP
 ?>
