a49e2144a680e0edfa37cc7b93fd2af24637ea97
HemalTharakaEranga <hemalgithub@gmail.com>
2024-06-03 11:43:41 +0530

    Define function

--- script.php
+++ script.php
@@ -8,7 +8,13 @@ echo "<h1>" .$str . "<h1>";
 echo "<h2>" .$num . "<h2>";
 echo "<h3>" .$float . "<h3>"; */ // multiple comments
 
-$cricket_teams = array("Sri Lanka","India","Australia","New Zeland");
+//$cricket_teams = array("Sri Lanka","India","Australia","New Zeland");
 //echo var_dump($cricket_teams); // this is the array use in PHP
-echo var_dump($cricket_teams[1]); // get the place of variable in the array. 
+//echo var_dump($cricket_teams[1]); // get the place of variable in the array. 
+
+//define("laptops","HP,Lenovo,Asus");//define function use
+//echo laptops;
+
+define("laptops",["HP","Lenovo","Asus"]);//define function called place
+echo laptops[2];
 ?>
